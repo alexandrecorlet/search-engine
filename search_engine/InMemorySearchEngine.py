a34bdd4c5710c215b1c39ab1dc219ac9a1430de7
alexandrecorlet <alexandre.santos@ccc.ufcg.edu.br>
2024-04-17 00:41:43 -0300

    fix: fixed some minor bugs

---
+++ search_engine/InMemorySearchEngine.py
@@ -0,0 +1,18 @@
+import csv
+from search_engine.Document import Document
+from search_engine.InvertedIndex import InvertedIndex
+
+class InMemorySearchEngine():
+    
+    def __init__(self, path_to_corpus):
+        self.corpus = self._load_corpus(path_to_corpus)
+        self.inverted_index = InvertedIndex(self.corpus)
+
+    def _load_corpus(self, path_to_corpus):
+        with open(path_to_corpus, 'r', encoding='utf-8') as csv_file:
+            corpus = [Document(doc_id, text) for doc_id, text in csv.reader(csv_file)]
+            return corpus
+
+    def search(self, query):
+        return self.inverted_index.get_top_k(query, top_k=5)
+
