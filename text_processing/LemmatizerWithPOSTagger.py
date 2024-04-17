c27e9855f74ded394215020ca7dba004adf65e4b
alexandrecorlet <alexandre.santos@ccc.ufcg.edu.br>
2024-04-16 23:09:41 -0300

    ADD: added lemmatizer with POS Tagger.

---
+++ text_processing/LemmatizerWithPOSTagger.py
@@ -0,0 +1,53 @@
+import nltk
+from nltk import WordNetLemmatizer
+from nltk.corpus import stopwords
+from nltk.corpus import wordnet
+
+class Splitter(object):
+
+    def __init__(self):
+        self.splitter = nltk.data.load('tokenizers/punkt/english.pickle')
+        self.tokenizer = nltk.tokenize.TreebankWordTokenizer()
+
+    def split(self,text):
+        # split into single sentence
+        sentences = self.splitter.tokenize(text)
+        # tokenization in each sentences
+        tokens = [self.tokenizer.tokenize(sent) for sent in sentences]
+        return tokens
+
+
+class LemmatizerWithPOSTagger(object):
+
+  def __init__(self):
+    self.lemmatizer = WordNetLemmatizer()
+    self.splitter = Splitter()
+
+    def get_wordnet_pos(self, treebank_tag):
+        if treebank_tag.startswith('A'):
+            return wordnet.ADJ
+        elif treebank_tag.startswith('V'):
+            return wordnet.VERB
+        elif treebank_tag.startswith('R'):
+            return wordnet.ADV
+        else:
+            # Default POS tagger is NOUN
+            return wordnet.NOUN
+
+    def pos_tag_lemma(self, text):
+        # Split the text into tokens
+        tokens = self.splitter.split(text)
+
+        # Flatten the list of tokens
+        tokens = [word for sentence in tokens for word in sentence]
+
+        # Find the pos tag for each token
+        pos_tokens = nltk.pos_tag(tokens)
+
+        # Lemmatization using pos tag
+        lemmas = []
+        for word, pos_tag in pos_tokens:
+            lemmas.append(self.lemmatizer.lemmatize(word, self.get_wordnet_pos(pos_tag)))
+
+        return lemmas
+
