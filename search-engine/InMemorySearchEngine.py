dd95b57d087a1f9920418a9bcdeb9f541b31f312
alexandrecorlet <alexandre.santos@ccc.ufcg.edu.br>
2024-04-17 00:21:42 -0300

    UPDATE: implement search function in in-memory search engine class.

--- search-engine/InMemorySearchEngine.py
+++ search-engine/InMemorySearchEngine.py
@@ -1,13 +1,18 @@
 import csv
 from Document import Document
+from InvertedIndex import InvertedIndex
 
 class InMemorySearchEngine():
     
     def __init__(self, path_to_corpus):
         self.corpus = self._load_corpus(path_to_corpus)
+        self.inverted_index = InvertedIndex(self.corpus)
 
     def _load_corpus(self, path_to_corpus):
         with open(path_to_corpus, 'r', encoding='utf-8') as csv_file:
             corpus = [Document(doc_id, text) for doc_id, text in csv.reader(csv_file)]
             return corpus
+
+    def search(self, query):
+        return self.inverted_index.get_top_k(query, k=5)
 
